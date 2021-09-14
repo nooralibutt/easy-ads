fce13543cd49eef1fe0468a08cda5a71b4c91af9
Abdullahjavaid0331 <89379686+Abdullahjavaid0331@users.noreply.github.com>
2021-09-14 12:07:43 +0500

    BannerAds apply successfully on project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:ads/screens/country_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(MyApp());
 }
 
--- lib/screens/country_list_screen.dart
+++ lib/screens/country_list_screen.dart
@@ -1,41 +1,75 @@
+import 'package:ads/helper/helper.dart';
 import 'package:ads/screens/country_detail_screen.dart';
 import 'package:ads/models/country.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class CountryListScreen extends StatelessWidget {
+class CountryListScreen extends StatefulWidget {
   const CountryListScreen({Key? key}) : super(key: key);
+
+  @override
+  _CountryListScreenState createState() => _CountryListScreenState();
+}
+
+class _CountryListScreenState extends State<CountryListScreen> {
+  late BannerAd bannerAds;
+
+  @override
+  void initState() {
+    super.initState();
+    bannerAds = Helper.create();
+    bannerAds.load();
+  }
+
+  @override
+  void dispose() {
+    bannerAds.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final countryList = Country.countryList;
+    final AdWidget adWidget = AdWidget(ad: bannerAds);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Country List"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-          itemCount: countryList.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      CountryDetailScreen(country: countryList[index]),
-                ),
-              ),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    Country.countryList[index].countryName,
-                    style: const TextStyle(
-                        fontSize: 28, fontWeight: FontWeight.w300),
-                  ),
-                ),
-              ),
-            );
-          }),
+      body: Column(
+        children: [
+          SizedBox(
+            child: adWidget,
+            height: 50,
+            width: double.infinity,
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: countryList.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            CountryDetailScreen(country: countryList[index]),
+                      ),
+                    ),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          Country.countryList[index].countryName,
+                          style: const TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }

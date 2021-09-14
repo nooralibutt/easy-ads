f48b947fee9604fc092a3ad024441ef3345eba44
Abdullahjavaid0331 <89379686+Abdullahjavaid0331@users.noreply.github.com>
2021-09-14 15:54:18 +0500

    myBannnerAds class  has been Successfully created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:ads/screens/country_list_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
   runApp(MyApp());
 }
 
---
+++ lib/myads/mybanner.dart
@@ -0,0 +1,19 @@
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class MyBannerAds {
+  static BannerAd bannerCreate() => BannerAd(
+        size: AdSize.banner,
+        adUnitId: BannerAd.testAdUnitId,
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) => print('Ad loaded has been Successfully bro.'),
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            ad.dispose();
+            print('Ad failed to load: $error');
+          },
+          onAdOpened: (Ad ad) => print('Ad opened.'),
+          onAdClosed: (Ad ad) => print('Ad closed.'),
+          onAdImpression: (Ad ad) => print('Ad impression.'),
+        ),
+        request: AdRequest(),
+      );
+}

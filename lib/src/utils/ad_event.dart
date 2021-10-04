d92acb09d8f17544a675574a54070595ac26df93
Ahmad Khan <ahmad.skstudios@gmail.com>
2021-10-04 20:45:05 +0500

    added comments in AdEvent & AdEventType

--- lib/src/utils/ad_event.dart
+++ lib/src/utils/ad_event.dart
@@ -2,6 +2,8 @@ import 'package:easy_ads_flutter/src/enums/ad_event_type.dart';
 import 'package:easy_ads_flutter/src/enums/ad_network.dart';
 import 'package:easy_ads_flutter/src/enums/ad_unit_type.dart';
 
+/// [AdEvent] is used to pass data inside event streams in easy ads instance
+/// You can use this to distinguish between different event types and each event type has a data attached to it.
 class AdEvent {
   final AdEventType type;
   final AdNetwork adNetwork;

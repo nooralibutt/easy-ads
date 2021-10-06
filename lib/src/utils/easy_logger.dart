080c3f024888c9abf2ea4eb442b78686bf73917f
Ahmad Khan <ahmad.skstudios@gmail.com>
2021-10-06 20:53:41 +0500

    Create EasyLogger and implement switch cases

---
+++ lib/src/utils/easy_logger.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+
+import 'package:easy_ads_flutter/easy_ads_flutter.dart';
+import 'package:logger/logger.dart';
+
+/// [EasyLogger] is used to listen to the callbacks in stream & show logs
+class EasyLogger {
+  /// [Logger] is used to show logs in console for EasyAds
+  final _logger = Logger();
+  StreamSubscription? streamSubscription;
+
+  void enable(bool enabled) {
+    streamSubscription?.cancel();
+    if (enabled) {
+      streamSubscription = EasyAds.instance.onEvent.listen(_onAdEvent);
+    }
+  }
+
+  void _onAdmobInitialized(AdEvent event) {
+    if (event.data == true) {
+      _logger.i(
+          "${event.adNetwork.value} has been initialized and is ready to use.");
+    } else {
+      _logger.e("${event.adNetwork.value} could not be initialized.");
+    }
+  }
+
+  void _onAdLoaded(AdEvent event) {
+    _logger.i(
+        "${event.adUnitType?.value} ads for ${event.adNetwork.value} have been loaded.");
+  }
+
+  void _onAdFailedToLoad(AdEvent event) {
+    _logger.e(
+        "${event.adUnitType?.value} ads for ${event.adNetwork.value} could not be loaded.");
+  }
+
+  void _onAdShowed(AdEvent event) {
+    _logger.i(
+        "${event.adUnitType?.value} ad for ${event.adNetwork.value} has been shown.");
+  }
+
+  void _onAdFailedShow(AdEvent event) {
+    _logger.e(
+        "${event.adUnitType?.value} ad for ${event.adNetwork.value} could not be showed.");
+  }
+
+  void _onAdDismissed(AdEvent event) {
+    _logger.i(
+        "${event.adUnitType?.value} ad for ${event.adNetwork.value} has been dismissed.");
+  }
+
+  void _onEarnedReward(AdEvent event) {
+    final dataMap = event.data as Map<String, dynamic>?;
+    _logger.i(
+        "User has earned ${dataMap?['rewardAmount']} of ${dataMap?['rewardType']} from ${event.adNetwork.value}");
+  }
+
+  void _onAdEvent(AdEvent event) {
+    switch (event.type) {
+      case AdEventType.adNetworkInitialized:
+        _onAdmobInitialized(event);
+        break;
+      case AdEventType.adLoaded:
+        _onAdLoaded(event);
+        break;
+      case AdEventType.adDismissed:
+        _onAdDismissed(event);
+        break;
+      case AdEventType.adShowed:
+        _onAdShowed(event);
+        break;
+      case AdEventType.adFailedToLoad:
+        _onAdFailedToLoad(event);
+        break;
+      case AdEventType.adFailedToShow:
+        _onAdFailedShow(event);
+        break;
+      case AdEventType.earnedReward:
+        _onEarnedReward(event);
+        break;
+    }
+  }
+}
